4a6524dab2d68f55005d6c99b09536904e1bb633
Unnikrishnan Patel <patel.unnikrishnan@gmail.com>
2016-04-01 22:35:21 +0530

    jury tests passed

---
+++ lib/jury.rb
@@ -0,0 +1,32 @@
+class Jury
+  attr_accessor :members,:finalists
+  def initialize
+    @members=[]
+    @finalists={}
+  end
+  def add_member(member)
+    @members << member
+  end
+  def cast_votes(finalists)
+    counter=0
+      finalists.each do |finalist|
+        @finalists[finalist] = 0
+      end
+      @members.each do |member|
+        vote = Random.new
+        vote=vote.rand(finalists.length)
+        puts @finalists[finalists[vote]]+=1
+      end
+      @finalists
+  end
+
+  def report_votes(final_votes)
+    final_votes.each do |finalist,votes|
+      puts "Finalist: #{finalist.to_s} | Votes: #{votes}"
+    end
+  end
+
+  def announce_winner(final_votes)
+    final_votes.each {|finalist,votes| return finalist if votes = final_votes.values.max}
+  end
+end
